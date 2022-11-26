badd70000be8adfb8c5fbdde57bfd11686ae6cd3
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-11-26 08:38:26 +0200

    bibitibi

--- init.py
+++ init.py
@@ -1,12 +1,33 @@
 import json
+import re
 from pathlib import Path
 
-disney = Path('disney')
-pairs = [
-    [(disney / 'output' / jpg.name).with_suffix('.d.jpg'), jpg]
-    for jpg in (disney / 'input').glob('*.jpg')]
+import requests
+from tqdm import tqdm
 
-pairs = [[str(o), str(i)] for o, i in pairs if o.exists() and i.exists()]
 
-with open('data.js', 'w') as f:
-    f.write('pairs = ' + json.dumps(pairs))
+def disney2data():
+    disney = Path('disney')
+    pairs = [
+        [(disney / 'output' / jpg.name).with_suffix('.d.jpg'), jpg]
+        for jpg in (disney / 'input').glob('*.jpg')]
+
+    pairs = [[str(o), str(i)] for o, i in pairs if o.exists() and i.exists()]
+
+    with open('data.js', 'w') as f:
+        f.write('pairs = ' + json.dumps(pairs))
+
+
+def download(url, path, i):
+    path = Path(path)
+    path.mkdir(parents=True, exist_ok=True)
+    with open(path / f'{i:03}.jpg', 'wb') as f:
+        f.write(requests.get(url).content)
+
+
+if __name__ == '__main__':
+    for name in ('bibi', 'tibi'):
+        with open(f'{name}.txt') as f:
+            urls = f.read().splitlines()
+            for i, url in tqdm(enumerate(urls)):
+                download(f'https:{url}', f'imgs/{name}/', i)
